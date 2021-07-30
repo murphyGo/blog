7f299dc4fda2d70287930f5dc67beb66acf97a7b
hyunseung.kim <murphy9333@gmail.com>
2021-07-31 03:16:12 +0900

    Initial commit

---
+++ src/test/java/org/github/murphygo/blog/ApplicationTest.java
@@ -0,0 +1,13 @@
+package org.github.murphygo.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    void applicationContextLoad() {
+        // Do nothing
+    }
+}
